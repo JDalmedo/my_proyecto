7778a21ffe6fbbe554ca5d012ba32f224db4df71
UsuarioM <jjdalmedo@gmail.com>
2023-07-20 13:08:30 +0200

    cambios2

--- data/projects.js
+++ data/projects.js
@@ -6,7 +6,7 @@ image:
 "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2VifGVufDB8fDB8fA",
 tech: ["React", "HTML5", "CSS3", "JavaScript"],
 github: "https://github.com/JDalmedo/my_proyecto",
-link: "www.proyecto1.com",
+link: "https://vercel.com/dashboard",
 },
 {
 title: "Segundo proyecto",
@@ -15,7 +15,7 @@ image:
 "https://images.unsplash.com/photo-1661961111184-11317b40adb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&a",
 tech: ["React", "HTML5", "CSS3", "JavaScript"],
 github: "http://github.com/JDalmedo/my_proyecto",
-link: "www.proyecto1.com",
+link: "https://vercel.com/dashboard",
 },
 {
 title: "Tercer proyecto",
@@ -24,7 +24,7 @@ image:
 "https://images.unsplash.com/photo-1669570094762-828f3dfaf675?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&a",
  tech: ["React", "HTML5", "CSS3", "JavaScript"],
 github: "https://github.com/JDalmedo/my_proyecto",
-link: "www.proyecto1.com",
+link: "https://vercel.com/dashboard",
 },
 {
 title: "Cuarto proyecto",
@@ -33,7 +33,7 @@ image:
 "https://images.unsplash.com/photo-1664575198308-3959904fa430?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&a",
 tech: ["React", "HTML5", "CSS3", "JavaScript"],
 github: "https://github.com/JDalmedo/my_proyecto",
-link: "www.proyecto1.com",
+link: "https://vercel.com/dashboard",
 },
 {
 title: "Quinto proyecto",
@@ -42,6 +42,6 @@ image:
 "https://images.unsplash.com/photo-1665686306574-1ace09918530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&a",
 tech: ["React", "HTML5", "CSS3", "JavaScript"],
 github: "https://github.com/JDalmedo/my_proyecto",
-link: "www.proyecto1.com",
+link: "https://vercel.com/dashboard",
 },
 ];
