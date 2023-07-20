d052df74738f79ffb3e83e6fb81142607bb27b26
UsuarioM <jjdalmedo@gmail.com>
2023-07-20 14:17:31 +0200

    cambios4

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -6,9 +6,9 @@ export const Home = () => {
   cleanPage(main);
   main.innerHTML = `
    <section class="home">
-   <p>Hey, I'm</p>
-   <h1>Juanjo Dalmedo</h1>
-   <p>I'm a software engineer based in Madrid, Spain and also a communication and journalism student. I enjoy creating things that
-   <a href="jjdalmedo@gmail.com">Say hi →</a>
+        <p>Hey, I'm</p>
+      <h1>Juanjo Dalmedo</h1>
+        <p>I'm a software engineer based in Madrid, Spain and also a communication and journalism student. I enjoy creating things that
+          <a href="mail to: jjdalmedo@gmail.com">Say hi →</a>
    </section>`;
 };
