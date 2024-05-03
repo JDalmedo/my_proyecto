404c597c3eb765342ad6568257af96f081682ca0
JDalmedo <jjdalmedo@gmail.com>
2024-05-03 18:14:09 +0200

    @

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -6,9 +6,9 @@ export const Home = () => {
   cleanPage(main);
   main.innerHTML = `
    <section class="home">
-        <p>Hey, I'm</p>
+        <p>Hola, mi nombre es Juan José Martínez Dalmedo, pero me gustan que me llamen Juanjo.</p>
       <h1>Juanjo Dalmedo</h1>
-        <p>I'm a software engineer based in Madrid, Spain and also a communication and journalism student. I enjoy creating things that
-          help people and I'm currently looking for a job as a developer web.</p>
+        <p>Soy un mecánico naval, apasionado por la tecnología y el mundo de la informática que está buscando su primera oportunidad laboral en el mundo del desarrollo web. Tengo una discapacidad debido a un accidente, pero muchas ganas de trabajar.
+        Aquí os dejo algunos de los proyectos que he creado.</p>
    </section>`;
 };
